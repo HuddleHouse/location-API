c9da044cf66f33cf0872750ad2f32bc4f99aede9
Matt Huddleston <mthuddleston@gmail.com>
2016-04-15 17:02:37 -0400

    Templating out the API

--- web/LocationApi/index.php
+++ web/LocationApi/index.php
@@ -5,17 +5,38 @@ use Symfony\Component\HttpFoundation\Response;
 
 
 $location = $app['controllers_factory'];
+
 $location->get('/save', function () use ($app) {
-
-
-    return "Welcome username!";
+    return new Response("Error: call POST instead of GET.", 401);
 });
 
-$location->post('/feedback', function (Request $request) {
-//    $message = $request->get('message');
-//    mail('feedback@yoursite.com', '[YourSite] Feedback', $message);
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'    => 'pdo_mysql',
+        'dbname'    => 'location_api',
+        'user'      => 'location_api',
+        'password'  => 'location_api',
+        'charset'   => 'utf8mb4',
+    ),
+));
 
-    return new Response('Thank you for your feedback!', 201);
+$location->post('/save', function (Request $request) {
+    $lat = $request->get('lat');
+    $lon = $request->get('lon');
+    $email = $request->get('email');
+    $time = $request->get('time');
+
+    try {
+        $sql = "insert into locations (lat, lon, email, time) values (?, ?, ?, ?)";
+
+        $location['db']['localhost']->executeUpdate($sql, array($lat, $lon, $email, $time));
+
+    }
+    catch (Exception $e) {
+        return 'Error: ' . $e->getMessage() ."\n";
+    }
+
+    return new Response('Success!', 201);
 });
 
 return $location;
